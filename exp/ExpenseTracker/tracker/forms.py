0ae77b64be71a237da432d0bb3c1065b49d68a69
Rprog-06 <145431249+Rprog-06@users.noreply.github.com>
2025-10-20 16:50:18 +0530

    Refactor forms.py to include Income model

--- exp/ExpenseTracker/tracker/forms.py
+++ exp/ExpenseTracker/tracker/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import Expense
+from .models import Expense,Income
 
 class ExpenseForm(forms.ModelForm):
     date = forms.DateField(
@@ -17,4 +17,5 @@ class IncomeForm(forms.ModelForm):
     class Meta:
         model = Income
         fields = ['amount']
+
 
