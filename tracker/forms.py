86e0b58448d4be3959ddc6ebfe7fb48c15ed4e50
Rizwan Vahora <vahorarizwan09@gmail.com>
2025-10-20 16:47:09 +0530

    Updated

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import Expense
-from .models import Income
+from .models import Expense,Income
 
 class ExpenseForm(forms.ModelForm):
     date = forms.DateField(
