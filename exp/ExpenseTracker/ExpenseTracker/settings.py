23e1ae3a43fd30cb8cb9c73db53814e70c2c9bb2
Rizwan Vahora <vahorarizwan09@gmail.com>
2025-12-02 14:47:58 +0530

    Updated

--- exp/ExpenseTracker/ExpenseTracker/settings.py
+++ exp/ExpenseTracker/ExpenseTracker/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-c**^2hjka71cyskfa68^ju+yznyd!7e4ckkd9@8-t@^ay3+!b%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['expense-tracker-10-1hmb.onrender.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['expense-tracker-1-3d8q.onrender.com','127.0.0.1', 'localhost']
 
 
 # Application definition
